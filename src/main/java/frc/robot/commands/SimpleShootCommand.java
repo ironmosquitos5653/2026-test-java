7616412557b22e466186f48dd3543e02aa7c0c30
wharrier <wharrier@yahoo.com>
2026-03-09 22:23:32 -0500

    updated speed

--- src/main/java/frc/robot/commands/SimpleShootCommand.java
+++ src/main/java/frc/robot/commands/SimpleShootCommand.java
@@ -122,9 +122,9 @@ public class SimpleShootCommand extends Command {
     }
 
     if (intakeIn) {
-      m_IntakeSubsystem.setDeploySpeed(.2);
+      m_IntakeSubsystem.setDeploySpeed(.4);
     } else {
-      m_IntakeSubsystem.setDeploySpeed(-.2);
+      m_IntakeSubsystem.setDeploySpeed(-.4);
     }
   }
 }
