8013d2e16fc854c4537a12deba29fc09240014e3
wharrier <wharrier@yahoo.com>
2026-04-09 20:06:24 -0500

    Removed old motors

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -14,24 +14,21 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
 
   private static final int shooterMotorCANId = 10;
-  private static final int shooter2MotorCANId = 12;
-  //private static final int shooter3MotorCANId = 11;
+
   private static final int advance1MotorCANId = 13;
   private static final int advance2MotorCANId = 14;
   private static final int hoodRotateMotorCANId = 17;
 
   private SparkFlex shooter1Motor;
-  private SparkFlex shooter2Motor;
-  //private SparkFlex shooter3Motor;
+
   private SparkFlex advance1Motor;
   private SparkFlex advance2Motor;
+
   private SparkFlex hoodRotateMotor;
   private PIDController hoodEncoderPidController;
   private AbsoluteEncoder hoodEncoder = null;
 
   FlywheelSubsystem fws1;
-  FlywheelSubsystem fws2;
-  //FlywheelSubsystem fws3;
 
   public static enum HoodPosition {
     DOWN(0.019),
@@ -50,15 +47,13 @@ public class ShooterSubsystem extends SubsystemBase {
 
   public ShooterSubsystem() {
     shooter1Motor = new SparkFlex(shooterMotorCANId, MotorType.kBrushless);
-    shooter2Motor = new SparkFlex(shooter2MotorCANId, MotorType.kBrushless);
-    //shooter3Motor = new SparkFlex(shooter3MotorCANId, MotorType.kBrushless);
+
     advance1Motor = new SparkFlex(advance1MotorCANId, MotorType.kBrushless);
     advance2Motor = new SparkFlex(advance2MotorCANId, MotorType.kBrushless);
+
     hoodRotateMotor = new SparkFlex(hoodRotateMotorCANId, MotorType.kBrushless);
 
     fws1 = new FlywheelSubsystem(shooter1Motor);
-    fws2 = new FlywheelSubsystem(shooter2Motor);
-    //fws3 = new FlywheelSubsystem(shooter3Motor);
 
     hoodEncoderPidController = new PIDController(12, 0, .5);
     hoodEncoderPidController.enableContinuousInput(0, 1);
@@ -69,8 +64,6 @@ public class ShooterSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     SmartDashboard.putNumber("RPM1", shooter1Motor.getEncoder().getVelocity());
-    SmartDashboard.putNumber("RPM2", shooter2Motor.getEncoder().getVelocity());
-    //SmartDashboard.putNumber("RPM3", shooter3Motor.getEncoder().getVelocity());
 
     hoodEncoderPidController.setSetpoint(hoodPosition);
     double speed = hoodEncoderPidController.calculate(hoodEncoder.getPosition());
@@ -85,8 +78,6 @@ public class ShooterSubsystem extends SubsystemBase {
 
   public void setShootSpeed(double speed) {
     fws1.setTargetRPM(-speed);
-    fws2.setTargetRPM(-speed);
-    //fws3.setTargetRPM(-speed);
   }
 
   public void setAdvanceSpeed(double speed) {
@@ -109,8 +100,6 @@ public class ShooterSubsystem extends SubsystemBase {
 
   public void dump() {
     SmartDashboard.putNumber("RPM 1 start", shooter1Motor.getEncoder().getVelocity());
-    SmartDashboard.putNumber("RPM 2 start", shooter2Motor.getEncoder().getVelocity());
-    //SmartDashboard.putNumber("RPM 3 start", shooter3Motor.getEncoder().getVelocity());
   }
 
   public boolean indexStuck() {
@@ -121,20 +110,11 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public double getVelocity() {
-    return shooter2Motor.getEncoder().getVelocity();
+    return shooter1Motor.getEncoder().getVelocity();
   }
 
   public double getShooterVelocity(int shooter) {
-    switch (shooter) {
-      case 0:
-        return shooter1Motor.getEncoder().getVelocity();
-      case 1:
-        return shooter2Motor.getEncoder().getVelocity();
-      case 2:
-        //return shooter3Motor.getEncoder().getVelocity();
-      default:
-        return 0.0;
-    }
+    return shooter1Motor.getEncoder().getVelocity();
   }
 
   public void deployClimb() {
